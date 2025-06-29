ee60ede266bfba7009d6d95e878bc7222d9f3eb0
Sandra Trinh <sandra_trinh@yahoo.com>
2025-06-30 00:20:10 +1000

    Set up a test text to see if input works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,79 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TextMeshProUGUI testInputText; // to print out input for testing
+    [SerializeField] private InputSystem_Actions _inputSystem; // the keybindings for the game
+    [SerializeField] private InputSystem_Actions.PlayerActions _playerActions; // the player's keybinding
+
+    private void Awake()
     {
-        
+        _inputSystem = new InputSystem_Actions();
+        _playerActions = _inputSystem.Player;
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        _playerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerActions.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        _inputSystem.Dispose();
+    }
+    
     void Update()
     {
-        
+        testInputText.text = "nothing is pressed";
+        UseLeftArm();
+        UseRightArm();
+        UseLeftLeg();
+        UseRightLeg();
+        UseBreathe();
+    }
+
+    public void UseLeftArm()
+    {
+        if (_playerActions.LeftArm.IsPressed())
+        {
+            testInputText.text = "Use Left Arm";
+        }
+    }
+    public void UseRightArm()
+    {
+        if (_playerActions.RightArm.IsPressed())
+        {
+            testInputText.text = "Use Right Arm";
+        }
+    }
+
+    public void UseLeftLeg()
+    {
+        if (_playerActions.LeftLeg.IsPressed())
+        {
+            testInputText.text = "Use Left Leg";
+        }
+    }
+    public void UseRightLeg()
+    {
+        if (_playerActions.RightLeg.IsPressed())
+        {
+            testInputText.text = "Use Right Leg";
+        }
+    }
+    
+    public void UseBreathe()
+    {
+        if (_playerActions.Breathe.IsPressed())
+        {
+            testInputText.text = "Breathe";
+        }
     }
 }
