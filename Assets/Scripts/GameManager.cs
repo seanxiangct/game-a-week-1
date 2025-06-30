90a621172f57d34071941be7ab3b2b862d2d662c
Sandra Trinh <sandra_trinh@yahoo.com>
2025-06-30 22:42:23 +1000

    set up the gamemanager as a singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,68 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //SINGLETON
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get {
+            if (_instance == null)
+            {
+                Debug.LogError("Game manager instance is null.");
+            }
+            return _instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            //destroy duplicates
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    //resets the fields to starting params
+    private void InitSetting()
+    {
+        //todo: set up the initial setting of the game.
+    }
+
+    /// <summary>
+    /// Starts the game
+    /// </summary>
+    public void StartGame()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Ends the game
+    /// </summary>
+    public void EndGame()
+    {
+        
+    }
+
+    /// <summary>
+    /// Pause game time
+    /// </summary>
+    public void PauseGame()
+    {
+        
+    }
+
+    /// <summary>
+    /// Start game time, continue play the game after pausing.
+    /// </summary>
+    public void UnPauseGame()
     {
         
     }
