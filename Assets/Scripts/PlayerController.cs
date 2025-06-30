23648acd9f9b04f7a64bb25cdab765d857b3192f
Sandra T <77083798+snowb-x@users.noreply.github.com>
2025-06-30 21:30:40 +1000

    Feature/player controller (#7)
    * Added WASDB keybinding for arms, legs and head/breathe to input system.
    
    * Set up a test text to see if input works
    
    * added check to see what we are holding/pressing at the same time
    
    * change the inputs keys to arms f/j, legs z/ and space for breathing
    
    followed what was in notion notes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,145 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TextMeshProUGUI testInputText; // to print out input for testing
+    [SerializeField] private InputSystem_Actions _inputSystem; // the keybindings for the game
+    [SerializeField] private InputSystem_Actions.PlayerActions _playerActions; // the player's keybinding
+    private bool keyPressed = false; 
+
+    private void Awake()
     {
-        
+        _inputSystem = new InputSystem_Actions();
+        _playerActions = _inputSystem.Player;
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        _playerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerActions.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        _inputSystem.Dispose();
+    }
+    
     void Update()
     {
-        
+        keyPressed = false; //reset this bool for this update.
+        testInputText.text = "nothing is pressed";
+        UseLeftArm();
+        UseRightArm();
+        UseLeftLeg();
+        UseRightLeg();
+        UseBreathe();
     }
+
+    /// <summary>
+    /// Left arm swimming action
+    /// </summary>
+    public void UseLeftArm()
+    {
+        if (_playerActions.LeftArm.IsPressed())
+        {
+            testInputText.text = "Use Left Arm";
+            keyPressedTrue();
+        }
+    }
+    /// <summary>
+    /// Right arm swimming action
+    /// </summary>
+    public void UseRightArm()
+    {
+        if (_playerActions.RightArm.IsPressed())
+        {
+            string newLine = "";
+            if (keyPressed)
+            {
+                newLine = "\n ";
+                testInputText.text += newLine + "Use Right Arm";
+            }
+            else
+            {
+                testInputText.text = "Use Right Arm";
+            }
+            
+            keyPressedTrue();
+        }
+    }
+
+    /// <summary>
+    /// Left leg swimming action
+    /// </summary>
+    public void UseLeftLeg()
+    {
+        if (_playerActions.LeftLeg.IsPressed())
+        {
+            string newLine = "";
+            if (keyPressed)
+            {
+                newLine = "\n ";
+                testInputText.text += newLine + "Use Left Leg";
+            }
+            else
+            {
+                testInputText.text ="Use Left Leg";
+            }
+            keyPressedTrue();
+        }
+    }
+    
+    /// <summary>
+    /// Right leg swimming action
+    /// </summary>
+    public void UseRightLeg()
+    {
+        if (_playerActions.RightLeg.IsPressed())
+        {
+            string newLine = "";
+            if (keyPressed)
+            {
+                newLine = "\n ";
+                testInputText.text += newLine + "Use Right Leg";
+            }
+            else
+            {
+                testInputText.text ="Use Right Leg";
+            }
+            keyPressedTrue();
+        }
+    }
+    
+    // need to hold to breathe air or drown.
+    public void UseBreathe()
+    {
+        if (_playerActions.Breathe.IsPressed())
+        {
+            string newLine = "";
+            if (keyPressed)
+            {
+                newLine = "\n ";
+                testInputText.text +=newLine + "Breathe";
+            }
+            else
+            {
+                testInputText.text = "Breathe";
+            }
+            
+            keyPressedTrue();
+        }
+    }
+
+    private void keyPressedTrue()
+    {
+        keyPressed = true;
+    }
+    
+    
 }
