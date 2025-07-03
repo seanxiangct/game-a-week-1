a842616dd2f792ed33611553d42b4b4ce5b67c39
Sandra Trinh <sandra_trinh@yahoo.com>
2025-07-03 17:11:27 +1000

    duck chases player

--- Assets/Scripts/DuckMovement.cs
+++ Assets/Scripts/DuckMovement.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class DuckMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Player _player;
+    private float angleBetween;
+    [SerializeField] private float speed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetDir = _player.transform.position - transform.position;
+        angleBetween = Vector3.Angle(transform.forward, targetDir);
+        transform.up = targetDir;
+        gameObject.transform.Translate((new Vector3(0,(speed*Time.deltaTime),0)));
     }
 }
